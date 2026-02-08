e43d08cdce96b5e4f9bf7a6b3d8826d986cc0d7b
riszart daryl <22.r.d.v.c.12@gmail.com>
2026-02-07 19:12:10 -0500

    version 0.1.0

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className='bg-amber-400 h-screen w-screen flex justify-center items-center flex-col'>
+      <h1 className='text-9xl uppercase font-bold'>z3dgina</h1>
+      <p className='text-4xl capitalize font-semibold bg-red-400 p-4 rounded-2xl'>pronto disponible</p>
+    </main>
+  )
+}
+
+export default App
